80091200aa2b0b510b350650d16270d296329840
niksa <niksaopfor@gmail.com>
2022-05-14 13:31:28 +0200

    Working or project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,12 @@
 // NAV MENU AND BUTTON
 const menuBtn = document.querySelector('.menu-btn');
 const navbarMenuBackground = document.querySelector('.navbar__menu-background');
-let menuOpen = false;
+
 
 menuBtn.addEventListener('click', function (e) {
     e.preventDefault();
     navbarMenuBackground.classList.toggle('menu-opend-closed');
 
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
     
 });
 
